3f52e945b090c6517c22a918cb37b89e72eb4bea
tangsharren <81765622+tangsharren@users.noreply.github.com>
2023-09-15 21:59:09 +0800

    Update staff.py

--- staff.py
+++ staff.py
@@ -47,24 +47,35 @@ def svLogin():
         if not supervisor_records:
             return render_template('StaffLogin.html', login_failed=True)
 
-        if supervisor_records and supervisor_records[0]['svPassword'] != svPassword:  # Use column name 'svPassword'
+        if supervisor_records and supervisor_records[0]['svPassword'] != svPassword:
             return render_template('StaffLogin.html', login_failed=True)
 
-        cursor.execute(fetch_student_sql, (svName,))  # Change 'svEmail' to 'svName'
+        cursor.execute(fetch_student_sql, (svName,))
         student_records = cursor.fetchall()
 
-        # Pass both supervisor and student records to the StaffPage template
-        return render_template('StaffPage.html', supervisor=supervisor_records[0], students=student_records)
+        # Generate URLs for student files from S3
+        student_records_url = {}
+        object_keys = ["com_acceptance_form", "parent_ack_form", "letter_of_indemnity", "hired_evidence"]
+        expiration = 3600
+
+        for student in student_records:
+            student_id = student['studId']
+            urls = {}
+            for key in object_keys:
+                object_key = f"stud-id-{student_id}/{student[key]}"
+                urls[key] = generate_presigned_url(object_key, expiration)
+            student_records_url[student_id] = urls
+
+        return render_template('StaffPage.html', supervisor=supervisor_records[0], students=student_records, urls=student_records_url)
 
     except Exception as e:
-        # Log the error or return a user-friendly error message
         app.logger.error(str(e))
         return "An error occurred."
 
     finally:
         cursor.close()
 
-
+        
 @app.route("/toSvLogin")
 def toSvLogin():
     return render_template('StaffLogin.html') 
